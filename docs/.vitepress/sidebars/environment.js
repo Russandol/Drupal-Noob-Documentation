d533ac1d87899f1df459bf58a4c04fdb68bce930
Sébastien <sebastien.clement@web-atrio.com>
2026-07-02 15:47:25 +0200

    save des vieux fichiers

--- docs/.vitepress/sidebars/environment.js
+++ docs/.vitepress/sidebars/environment.js
@@ -1,16 +1,16 @@
-export default {
-  text: 'Environnement de développement',
-  collapsed: true,
-  items: [
-    { text: 'Introduction', link: '/01-environnement-developpement/' },
-    { text: 'Docker compose', link: '/01-environnement-developpement/compose' },
-    { text: 'Fichiers d\'environnement', link: '/01-environnement-developpement/env-files' },
-    { text: 'PHP ini', link: '/01-environnement-developpement/php-ini' },
-    { text: 'Dockerfile', link: '/01-environnement-developpement/dockerfile' },
-    { text: 'Environnement de production', link: '/01-environnement-developpement/compose-prod' },
-    { text: 'Fichiers ignore', link: '/01-environnement-developpement/ignore-files' },
-    { text: 'Lignes de commandes', link: '/01-environnement-developpement/commands' },
-    { text: 'Makefile', link: '/01-environnement-developpement/makefile' },
-    { text: 'Readme', link: '/01-environnement-developpement/readme' },
-  ]
-}
+// export default {
+//   text: 'Environnement de développement',
+//   collapsed: true,
+//   items: [
+//     { text: 'Introduction', link: '/01-environnement-developpement/' },
+//     { text: 'Docker compose', link: '/01-environnement-developpement/compose' },
+//     { text: 'Fichiers d\'environnement', link: '/01-environnement-developpement/env-files' },
+//     { text: 'PHP ini', link: '/01-environnement-developpement/php-ini' },
+//     { text: 'Dockerfile', link: '/01-environnement-developpement/dockerfile' },
+//     { text: 'Environnement de production', link: '/01-environnement-developpement/compose-prod' },
+//     { text: 'Fichiers ignore', link: '/01-environnement-developpement/ignore-files' },
+//     { text: 'Lignes de commandes', link: '/01-environnement-developpement/commands' },
+//     { text: 'Makefile', link: '/01-environnement-developpement/makefile' },
+//     { text: 'Readme', link: '/01-environnement-developpement/readme' },
+//   ]
+// }
--- docs/.vitepress/sidebars/project.js
+++ docs/.vitepress/sidebars/project.js
@@ -1,9 +1,9 @@
-export default {
-  text: 'Le projet',
-  collapsed: true,
-  items: [
-    { text: 'Objectif de la formation', link: '/project/' },
-    { text: 'Cinecritique', link: '/project/cinecritique' },
-    { text: 'Le fichier Readme', link: '/project/readme' },
-  ]
-}
+// export default {
+//   text: 'Le projet',
+//   collapsed: true,
+//   items: [
+//     { text: 'Objectif de la formation', link: '/project/' },
+//     { text: 'Cinecritique', link: '/project/cinecritique' },
+//     { text: 'Le fichier Readme', link: '/project/readme' },
+//   ]
+// }
